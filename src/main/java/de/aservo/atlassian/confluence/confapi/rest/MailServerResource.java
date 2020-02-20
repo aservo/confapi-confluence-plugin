tied/confluence-confapi-plugin

Implement settings and mail REST endpoints
This commit provides the initial implementation of the Confluence confapi
plugin. The first provided REST endpoints are for providing and accessing
configuration for general settings and mail servers.

There is still missing:

* Proper documentation in the README file
* Proper javadoc
* Restriction of the REST API to sysadmins
* Some unit tests for full test coverage
* Integration tests
* Usage of the ASERVO parent pom

src/main/java/de/aservo/atlassian/confluence/confapi/rest/MailServerResource.java
@@ -1,0 +1,212 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import com.atlassian.mail.MailException;
+import com.atlassian.mail.MailProtocol;
+import com.atlassian.mail.server.MailServerManager;
+import com.atlassian.mail.server.PopMailServer;
+import com.atlassian.mail.server.SMTPMailServer;
+import com.atlassian.mail.server.impl.PopMailServerImpl;
+import com.atlassian.mail.server.impl.SMTPMailServerImpl;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import de.aservo.atlassian.confluence.confapi.bean.PopMailServerBean;
+import de.aservo.atlassian.confluence.confapi.bean.SmtpMailServerBean;
+import de.aservo.atlassian.confluence.confapi.bean.error.ErrorCollection;
+import de.aservo.atlassian.confluence.confapi.util.MailProtocolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource to set mail server configuration.
+ */
+@Path("/mail")
+@Produces(MediaType.APPLICATION_JSON)
+// TODO: Implement and enable SysadminOnlyResourceFilter
+// @ResourceFilters(SysadminOnlyResourceFilter.class)
+@Named
+public class MailServerResource {
+
+    private static final Logger log = LoggerFactory.getLogger(MailServerResource.class);
+
+    @ComponentImport
+    private final MailServerManager mailServerManager;
+
+    /**
+     * Constructor.
+     *
+     * @param mailServerManager the injected {@link MailServerManager}
+     */
+    @Inject
+    public MailServerResource(
+            final MailServerManager mailServerManager) {
+
+        this.mailServerManager = mailServerManager;
+    }
+
+    @GET
+    @Path("smtp")
+    public Response getSmtpMailServer() {
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        try {
+            final SMTPMailServer smtpMailServer = mailServerManager.getDefaultSMTPMailServer();
+            final SmtpMailServerBean bean = SmtpMailServerBean.from(smtpMailServer);
+            return Response.ok(bean).build();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).entity(errorCollection).build();
+    }
+
+    @PUT
+    @Path("smtp")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putSmtpMailServer(
+            final SmtpMailServerBean bean) {
+
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        final SMTPMailServer smtpMailServer = mailServerManager.isDefaultSMTPMailServerDefined()
+                ? mailServerManager.getDefaultSMTPMailServer()
+                : new SMTPMailServerImpl();
+
+        assert smtpMailServer != null;
+
+        if (StringUtils.isNotBlank(bean.getName())) {
+            smtpMailServer.setName(bean.getName());
+        }
+
+        if (StringUtils.isNotBlank(bean.getDescription())) {
+            smtpMailServer.setDescription(bean.getDescription());
+        }
+
+        if (StringUtils.isNotBlank(bean.getFrom())) {
+            smtpMailServer.setDefaultFrom(bean.getFrom());
+        }
+
+        if (StringUtils.isNotBlank(bean.getPrefix())) {
+            smtpMailServer.setPrefix(bean.getPrefix());
+        }
+
+        smtpMailServer.setMailProtocol(MailProtocolUtil.find(bean.getProtocol(), MailProtocol.SMTP));
+
+        if (StringUtils.isNotBlank(bean.getHost())) {
+            smtpMailServer.setHostname(bean.getHost());
+        }
+
+        if (bean.getPort() != null) {
+            smtpMailServer.setPort(String.valueOf(bean.getPort()));
+        } else {
+            smtpMailServer.setPort(smtpMailServer.getMailProtocol().getDefaultPort());
+        }
+
+        smtpMailServer.setTlsRequired(bean.isTls());
+
+        if (StringUtils.isNotBlank(bean.getUsername())) {
+            smtpMailServer.setUsername(bean.getUsername());
+        }
+
+        smtpMailServer.setTimeout(bean.getTimeout());
+
+        try {
+            if (mailServerManager.isDefaultSMTPMailServerDefined()) {
+                mailServerManager.update(smtpMailServer);
+            } else {
+                smtpMailServer.setId(mailServerManager.create(smtpMailServer));
+            }
+
+            return Response.ok(bean).build();
+        } catch (MailException e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorCollection).build();
+    }
+
+    @GET
+    @Path("pop")
+    public Response getPopMailServer() {
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        try {
+            final PopMailServer popMailServer = mailServerManager.getDefaultPopMailServer();
+            final PopMailServerBean bean = PopMailServerBean.from(popMailServer);
+            return Response.ok(bean).build();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).entity(errorCollection).build();
+    }
+
+    @PUT
+    @Path("pop")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putPopMailServer(
+            final PopMailServerBean bean) {
+
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        final PopMailServer popMailServer = mailServerManager.getDefaultPopMailServer() != null
+                ? mailServerManager.getDefaultPopMailServer()
+                : new PopMailServerImpl();
+
+        assert popMailServer != null;
+
+        if (StringUtils.isNotBlank(bean.getName())) {
+            popMailServer.setName(bean.getName());
+        }
+
+        if (StringUtils.isNotBlank(bean.getDescription())) {
+            popMailServer.setDescription(bean.getDescription());
+        }
+
+        popMailServer.setMailProtocol(MailProtocolUtil.find(bean.getProtocol(), MailProtocol.POP));
+
+        if (StringUtils.isNotBlank(bean.getHost())) {
+            popMailServer.setHostname(bean.getHost());
+        }
+
+        if (bean.getPort() != null) {
+            popMailServer.setPort(String.valueOf(bean.getPort()));
+        } else {
+            popMailServer.setPort(popMailServer.getMailProtocol().getDefaultPort());
+        }
+
+        if (StringUtils.isNotBlank(bean.getUsername())) {
+            popMailServer.setUsername(bean.getUsername());
+        }
+
+        popMailServer.setTimeout(bean.getTimeout());
+
+        try {
+            if (mailServerManager.getDefaultPopMailServer() != null) {
+                mailServerManager.update(popMailServer);
+            } else {
+                popMailServer.setId(mailServerManager.create(popMailServer));
+            }
+
+            return Response.ok(bean).build();
+        } catch (MailException e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorCollection).build();
+    }
+
+}

src/main/java/de/aservo/atlassian/confluence/confapi/rest/SettingsResource.java
@@ -1,0 +1,69 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import com.atlassian.confluence.setup.settings.Settings;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import de.aservo.atlassian.confluence.confapi.bean.SettingsBean;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource to set general configuration.
+ */
+@Path("/settings")
+@Produces({MediaType.APPLICATION_JSON})
+// TODO: Implement and enable SysadminOnlyResourceFilter
+// @ResourceFilters(SysadminOnlyResourceFilter.class)
+@Named("SettingsResource")
+public class SettingsResource {
+
+    @ComponentImport
+    private final SettingsManager settingsManager;
+
+    @Inject
+    public SettingsResource(
+            final SettingsManager settingsManager) {
+
+        this.settingsManager = settingsManager;
+    }
+
+    @GET
+    public Response getSettings() {
+        final Settings settings = settingsManager.getGlobalSettings();
+
+        final SettingsBean settingsBean = new SettingsBean(
+                settings.getBaseUrl(),
+                settings.getSiteTitle()
+        );
+
+        return Response.ok(settingsBean).build();
+    }
+
+    @PUT
+    public Response putSettings(
+            final SettingsBean bean) {
+
+        final Settings settings = settingsManager.getGlobalSettings();
+
+        if (StringUtils.isNotBlank(bean.getBaseurl())) {
+            settings.setBaseUrl(bean.getBaseurl());
+        }
+
+        if (StringUtils.isNotBlank(bean.getTitle())) {
+            settings.setSiteTitle(bean.getTitle());
+        }
+
+        settingsManager.updateGlobalSettings(settings);
+
+        return Response.ok(SettingsBean.from(settings)).build();
+    }
+
+}

src/test/java/de/aservo/atlassian/confluence/confapi/bean/PopMailServerBeanTest.java
@@ -1,0 +1,78 @@
+package de.aservo.atlassian.confluence.confapi.bean;
+
+import com.atlassian.mail.server.DefaultTestPopMailServerImpl;
+import com.atlassian.mail.server.PopMailServer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PopMailServerBeanTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        final PopMailServerBean bean = new PopMailServerBean();
+
+        assertNull(bean.getName());
+        assertNull(bean.getDescription());
+        assertNull(bean.getProtocol());
+        assertNull(bean.getHost());
+        assertNull(bean.getPort());
+        assertEquals(bean.getTimeout(), PopMailServerBean.DEFAULT_TIMEOUT);
+        assertNull(bean.getUsername());
+        assertNull(bean.getPassword());
+    }
+
+    @Test
+    public void testFromConstructor() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        final PopMailServerBean bean = PopMailServerBean.from(server);
+
+        assertEquals(bean.getName(), server.getName());
+        assertEquals(bean.getDescription(), server.getDescription());
+        assertEquals(bean.getProtocol(), server.getMailProtocol().getProtocol());
+        assertEquals(bean.getHost(), server.getHostname());
+        assertEquals(bean.getPort(), Integer.valueOf(server.getPort()));
+        assertEquals(bean.getTimeout(), server.getTimeout());
+        assertEquals(bean.getUsername(), server.getUsername());
+        // assertEquals(bean.getPassword(), server.getPassword());
+    }
+
+    @Test
+    public void testFromConstructorHideEmptyDescription() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        server.setDescription("");
+        final PopMailServerBean bean = PopMailServerBean.from(server);
+
+        assertNull(bean.getDescription());
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        final PopMailServerBean bean1 = PopMailServerBean.from(server);
+        final PopMailServerBean bean2 = PopMailServerBean.from(server);
+        assertEquals(bean1, bean2);
+    }
+
+    @Test
+    public void testEqualsNull() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertFalse(bean.equals(null));
+    }
+
+    @Test
+    public void testEqualsSameInstance() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertEquals(bean, bean);
+    }
+
+    @Test
+    public void testEqualsOtherType() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertFalse(bean.equals(new Object()));
+    }
+
+}

aservo/confapi-confluence-plugin

src/main/java/de/aservo/atlassian/confluence/confapi/rest/MailServerResource.java
@@ -1,0 +1,210 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import com.atlassian.mail.MailException;
+import com.atlassian.mail.MailProtocol;
+import com.atlassian.mail.server.MailServerManager;
+import com.atlassian.mail.server.PopMailServer;
+import com.atlassian.mail.server.SMTPMailServer;
+import com.atlassian.mail.server.impl.PopMailServerImpl;
+import com.atlassian.mail.server.impl.SMTPMailServerImpl;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import de.aservo.atlassian.confluence.confapi.model.PopMailServerBean;
+import de.aservo.atlassian.confluence.confapi.model.SmtpMailServerBean;
+import de.aservo.atlassian.confluence.confapi.model.ErrorCollection;
+import de.aservo.atlassian.confluence.confapi.util.MailProtocolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource to set mail server configuration.
+ */
+@Path("/mail")
+@Produces(MediaType.APPLICATION_JSON)
+@Named
+public class MailServerResource {
+
+    private static final Logger log = LoggerFactory.getLogger(MailServerResource.class);
+
+    @ComponentImport
+    private final MailServerManager mailServerManager;
+
+    /**
+     * Constructor.
+     *
+     * @param mailServerManager the injected {@link MailServerManager}
+     */
+    @Inject
+    public MailServerResource(
+            final MailServerManager mailServerManager) {
+
+        this.mailServerManager = mailServerManager;
+    }
+
+    @GET
+    @Path("smtp")
+    public Response getSmtpMailServer() {
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        try {
+            final SMTPMailServer smtpMailServer = mailServerManager.getDefaultSMTPMailServer();
+            final SmtpMailServerBean bean = SmtpMailServerBean.from(smtpMailServer);
+            return Response.ok(bean).build();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).entity(errorCollection).build();
+    }
+
+    @PUT
+    @Path("smtp")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putSmtpMailServer(
+            final SmtpMailServerBean bean) {
+
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        final SMTPMailServer smtpMailServer = mailServerManager.isDefaultSMTPMailServerDefined()
+                ? mailServerManager.getDefaultSMTPMailServer()
+                : new SMTPMailServerImpl();
+
+        assert smtpMailServer != null;
+
+        if (StringUtils.isNotBlank(bean.getName())) {
+            smtpMailServer.setName(bean.getName());
+        }
+
+        if (StringUtils.isNotBlank(bean.getDescription())) {
+            smtpMailServer.setDescription(bean.getDescription());
+        }
+
+        if (StringUtils.isNotBlank(bean.getFrom())) {
+            smtpMailServer.setDefaultFrom(bean.getFrom());
+        }
+
+        if (StringUtils.isNotBlank(bean.getPrefix())) {
+            smtpMailServer.setPrefix(bean.getPrefix());
+        }
+
+        smtpMailServer.setMailProtocol(MailProtocolUtil.find(bean.getProtocol(), MailProtocol.SMTP));
+
+        if (StringUtils.isNotBlank(bean.getHost())) {
+            smtpMailServer.setHostname(bean.getHost());
+        }
+
+        if (bean.getPort() != null) {
+            smtpMailServer.setPort(String.valueOf(bean.getPort()));
+        } else {
+            smtpMailServer.setPort(smtpMailServer.getMailProtocol().getDefaultPort());
+        }
+
+        smtpMailServer.setTlsRequired(bean.isTls());
+
+        if (StringUtils.isNotBlank(bean.getUsername())) {
+            smtpMailServer.setUsername(bean.getUsername());
+        }
+
+        smtpMailServer.setTimeout(bean.getTimeout());
+
+        try {
+            if (mailServerManager.isDefaultSMTPMailServerDefined()) {
+                mailServerManager.update(smtpMailServer);
+            } else {
+                smtpMailServer.setId(mailServerManager.create(smtpMailServer));
+            }
+
+            return Response.ok(bean).build();
+        } catch (MailException e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorCollection).build();
+    }
+
+    @GET
+    @Path("pop")
+    public Response getPopMailServer() {
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        try {
+            final PopMailServer popMailServer = mailServerManager.getDefaultPopMailServer();
+            final PopMailServerBean bean = PopMailServerBean.from(popMailServer);
+            return Response.ok(bean).build();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).entity(errorCollection).build();
+    }
+
+    @PUT
+    @Path("pop")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putPopMailServer(
+            final PopMailServerBean bean) {
+
+        final ErrorCollection errorCollection = new ErrorCollection();
+
+        final PopMailServer popMailServer = mailServerManager.getDefaultPopMailServer() != null
+                ? mailServerManager.getDefaultPopMailServer()
+                : new PopMailServerImpl();
+
+        assert popMailServer != null;
+
+        if (StringUtils.isNotBlank(bean.getName())) {
+            popMailServer.setName(bean.getName());
+        }
+
+        if (StringUtils.isNotBlank(bean.getDescription())) {
+            popMailServer.setDescription(bean.getDescription());
+        }
+
+        popMailServer.setMailProtocol(MailProtocolUtil.find(bean.getProtocol(), MailProtocol.POP));
+
+        if (StringUtils.isNotBlank(bean.getHost())) {
+            popMailServer.setHostname(bean.getHost());
+        }
+
+        if (bean.getPort() != null) {
+            popMailServer.setPort(String.valueOf(bean.getPort()));
+        } else {
+            popMailServer.setPort(popMailServer.getMailProtocol().getDefaultPort());
+        }
+
+        if (StringUtils.isNotBlank(bean.getUsername())) {
+            popMailServer.setUsername(bean.getUsername());
+        }
+
+        popMailServer.setTimeout(bean.getTimeout());
+
+        try {
+            if (mailServerManager.getDefaultPopMailServer() != null) {
+                mailServerManager.update(popMailServer);
+            } else {
+                popMailServer.setId(mailServerManager.create(popMailServer));
+            }
+
+            return Response.ok(bean).build();
+        } catch (MailException e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorCollection).build();
+    }
+
+}

src/main/java/de/aservo/atlassian/confluence/confapi/rest/SettingsResource.java
@@ -1,0 +1,67 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import com.atlassian.confluence.setup.settings.Settings;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import de.aservo.atlassian.confluence.confapi.model.SettingsBean;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource to set general configuration.
+ */
+@Path("/settings")
+@Produces({MediaType.APPLICATION_JSON})
+@Named("SettingsResource")
+public class SettingsResource {
+
+    @ComponentImport
+    private final SettingsManager settingsManager;
+
+    @Inject
+    public SettingsResource(
+            final SettingsManager settingsManager) {
+
+        this.settingsManager = settingsManager;
+    }
+
+    @GET
+    public Response getSettings() {
+        final Settings settings = settingsManager.getGlobalSettings();
+
+        final SettingsBean settingsBean = new SettingsBean(
+                settings.getBaseUrl(),
+                settings.getSiteTitle()
+        );
+
+        return Response.ok(settingsBean).build();
+    }
+
+    @PUT
+    public Response putSettings(
+            final SettingsBean bean) {
+
+        final Settings settings = settingsManager.getGlobalSettings();
+
+        if (StringUtils.isNotBlank(bean.getBaseurl())) {
+            settings.setBaseUrl(bean.getBaseurl());
+        }
+
+        if (StringUtils.isNotBlank(bean.getTitle())) {
+            settings.setSiteTitle(bean.getTitle());
+        }
+
+        settingsManager.updateGlobalSettings(settings);
+
+        return Response.ok(SettingsBean.from(settings)).build();
+    }
+
+}

src/test/java/de/aservo/atlassian/confluence/confapi/model/PopMailServerBeanTest.java
@@ -1,0 +1,79 @@
+package de.aservo.atlassian.confluence.confapi.model;
+
+import com.atlassian.mail.server.DefaultTestPopMailServerImpl;
+import com.atlassian.mail.server.PopMailServer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static de.aservo.atlassian.confluence.confapi.model.PopMailServerBean.DEFAULT_TIMEOUT;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PopMailServerBeanTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        final PopMailServerBean bean = new PopMailServerBean();
+
+        assertNull(bean.getName());
+        assertNull(bean.getDescription());
+        assertNull(bean.getProtocol());
+        assertNull(bean.getHost());
+        assertNull(bean.getPort());
+        assertEquals(DEFAULT_TIMEOUT, bean.getTimeout());
+        assertNull(bean.getUsername());
+        assertNull(bean.getPassword());
+    }
+
+    @Test
+    public void testFromConstructor() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        final PopMailServerBean bean = PopMailServerBean.from(server);
+
+        assertEquals(bean.getName(), server.getName());
+        assertEquals(bean.getDescription(), server.getDescription());
+        assertEquals(bean.getProtocol(), server.getMailProtocol().getProtocol());
+        assertEquals(bean.getHost(), server.getHostname());
+        assertEquals(bean.getPort(), Integer.valueOf(server.getPort()));
+        assertEquals(bean.getTimeout(), server.getTimeout());
+        assertEquals(bean.getUsername(), server.getUsername());
+        // assertEquals(bean.getPassword(), server.getPassword());
+    }
+
+    @Test
+    public void testFromConstructorHideEmptyDescription() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        server.setDescription("");
+        final PopMailServerBean bean = PopMailServerBean.from(server);
+
+        assertNull(bean.getDescription());
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        final PopMailServer server = new DefaultTestPopMailServerImpl();
+        final PopMailServerBean bean1 = PopMailServerBean.from(server);
+        final PopMailServerBean bean2 = PopMailServerBean.from(server);
+        assertEquals(bean1, bean2);
+    }
+
+    @Test
+    public void testEqualsNull() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertFalse(bean.equals(null));
+    }
+
+    @Test
+    public void testEqualsSameInstance() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertEquals(bean, bean);
+    }
+
+    @Test
+    public void testEqualsOtherType() {
+        final PopMailServerBean bean = new PopMailServerBean();
+        assertFalse(bean.equals(new Object()));
+    }
+
+}
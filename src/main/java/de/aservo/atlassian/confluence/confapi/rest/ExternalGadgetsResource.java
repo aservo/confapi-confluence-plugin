0cec69b498d1d7473e7002e570f22ac173872f08
Kai Lehmann <kai.uni-assist@lekatech.de>
2020-02-28 15:41:38 +0100

    COS18 implementation for external gadgets

---
+++ src/main/java/de/aservo/atlassian/confluence/confapi/rest/ExternalGadgetsResource.java
@@ -0,0 +1,63 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import de.aservo.atlassian.confluence.confapi.model.ErrorCollection;
+import de.aservo.atlassian.confluence.confapi.service.ExternalGadgetsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+
+/**
+ * Resource to set external gadgets configuration.
+ */
+@Path("/gadgets/external")
+@Produces(MediaType.APPLICATION_JSON)
+@Named
+public class ExternalGadgetsResource {
+
+    private static final Logger log = LoggerFactory.getLogger(ExternalGadgetsResource.class);
+
+    private final ExternalGadgetsService externalGadgetsService;
+
+    /**
+     * Constructor.
+     *
+     * @param externalGadgetsService the injected {@link ExternalGadgetsService}
+     */
+    public ExternalGadgetsResource(ExternalGadgetsService externalGadgetsService) {
+        this.externalGadgetsService = externalGadgetsService;
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getGadgets() {
+        final ErrorCollection errorCollection = new ErrorCollection();
+        try {
+            return Response.ok(externalGadgetsService.getRegisteredExternalGadgetURls()).build();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+        return Response.status(BAD_REQUEST).entity(errorCollection).build();
+    }
+
+    @PUT
+    @Consumes({MediaType.TEXT_PLAIN})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response addGadget(@QueryParam("test") Boolean testGadgetUrl, String gadgetUrl) {
+        final ErrorCollection errorCollection = new ErrorCollection();
+        try {
+            externalGadgetsService.addExternalGadgetUrl(gadgetUrl, testGadgetUrl);
+            return getGadgets();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            errorCollection.addErrorMessage(e.getMessage());
+        }
+        return Response.status(BAD_REQUEST).entity(errorCollection).build();
+    }
+}
---
+++ src/main/java/de/aservo/atlassian/confluence/confapi/service/ExternalGadgetsService.java
@@ -0,0 +1,97 @@
+package de.aservo.atlassian.confluence.confapi.service;
+
+import com.atlassian.confluence.languages.LocaleManager;
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
+import com.atlassian.confluence.user.ConfluenceUser;
+import com.atlassian.gadgets.GadgetParsingException;
+import com.atlassian.gadgets.GadgetRequestContext;
+import com.atlassian.gadgets.directory.spi.ExternalGadgetSpec;
+import com.atlassian.gadgets.directory.spi.ExternalGadgetSpecStore;
+import com.atlassian.gadgets.spec.GadgetSpecFactory;
+import com.atlassian.plugin.spring.scanner.annotation.component.ConfluenceComponent;
+import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * The type External gadgets service. This work is insipred by class AddOrRemoveGadgetSpecAction of plugin file gadgets-plugin-9.0.4.jar
+ *
+ * Note: if ever needed: com.atlassian.gadgets.GadgetSpecProvider provides internal gadget specs
+ */
+@ExportAsService
+@ConfluenceComponent
+public class ExternalGadgetsService {
+
+    private static final Logger log = LoggerFactory.getLogger(ExternalGadgetsService.class);
+
+    private final ExternalGadgetSpecStore externalGadgetSpecStore;
+    private final GadgetSpecFactory gadgetSpecFactory;
+    private final LocaleManager localeManager;
+
+    /**
+     * Instantiates a new External gadgets service.
+     *
+     * @param externalGadgetSpecStore the external gadget spec store
+     * @param gadgetSpecFactory       the gadget spec factory
+     * @param localeManager           the locale manager
+     */
+    public ExternalGadgetsService(@ComponentImport ExternalGadgetSpecStore externalGadgetSpecStore,
+                                  @ComponentImport GadgetSpecFactory gadgetSpecFactory,
+                                  @ComponentImport LocaleManager localeManager) {
+        this.externalGadgetSpecStore = externalGadgetSpecStore;
+        this.gadgetSpecFactory = gadgetSpecFactory;
+        this.localeManager = localeManager;
+    }
+
+    /**
+     * Gets registered external gadget urls.
+     *
+     * @return the registered external gadget urls
+     */
+    public List<String> getRegisteredExternalGadgetURls() {
+        Iterable<ExternalGadgetSpec> specIterable = externalGadgetSpecStore.entries();
+        return StreamSupport.stream(specIterable.spliterator(), false)
+                .map(spec -> spec.getSpecUri().toString()).collect(Collectors.toList());
+    }
+
+    /**
+     * Add external gadget url.
+     *
+     * @param url           the url
+     * @param testGadgetUrl whether to test gadget url
+     * @throws URISyntaxException the uri syntax exception
+     */
+    public void addExternalGadgetUrl(String url, Boolean testGadgetUrl) throws URISyntaxException {
+
+        //initial checks
+        assert url != null && StringUtils.isNotBlank(url.trim());
+        String gadgetUrlToAdd = url.trim();
+        URI uri = new URI(gadgetUrlToAdd);
+
+        //validate gadget url
+        if (testGadgetUrl == null || testGadgetUrl) {
+            ConfluenceUser user = AuthenticatedUserThreadLocal.get();
+            Locale locale = localeManager.getLocale(user);
+            GadgetRequestContext requestContext = GadgetRequestContext.Builder.gadgetRequestContext().locale(locale)
+                    .ignoreCache(false).user(new GadgetRequestContext.User(user.getKey().getStringValue(), user.getName())).build();
+            try {
+                gadgetSpecFactory.getGadgetSpec(uri, requestContext);
+            } catch (GadgetParsingException e) {
+                log.warn("Could not add gadget at {}", uri, e);
+                throw e;
+            }
+        }
+
+        //add gadget url to store
+        externalGadgetSpecStore.add(uri);
+    }
+}

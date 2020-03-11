b6f629c587e6202e758f29e042c33d07e211c068
Kai Lehmann <kai.uni-assist@lekatech.de>
2020-03-11 09:50:54 +0100

    extended email field setter with backwards compatiobility towardas confluence 6.13.10

--- src/main/java/de/aservo/atlassian/confluence/confapi/service/UserService.java
+++ src/main/java/de/aservo/atlassian/confluence/confapi/service/UserService.java
@@ -1,6 +1,7 @@
 package de.aservo.atlassian.confluence.confapi.service;
 
 import com.atlassian.confluence.user.ConfluenceUserImpl;
+import com.atlassian.crowd.embedded.atlassianuser.EmbeddedCrowdUser;
 import com.atlassian.crowd.exception.UserNotFoundException;
 import com.atlassian.plugin.spring.scanner.annotation.component.ConfluenceComponent;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
@@ -9,6 +10,7 @@ import com.atlassian.user.EntityException;
 import com.atlassian.user.User;
 import com.atlassian.user.UserManager;
 import de.aservo.atlassian.confluence.confapi.model.UserBean;
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,13 +61,27 @@ public class UserService {
      * @throws IllegalAccessException the illegal access exception
      * @throws UserNotFoundException  the user not found exception
      */
-    public UserBean updateUser(UserBean user) throws EntityException, IllegalAccessException, UserNotFoundException {
+    public UserBean updateUser(UserBean user) throws EntityException, UserNotFoundException, IllegalAccessException {
         BeanValidationService.validate(user);
         User atlUser = userManager.getUser(user.getUsername());
         if (atlUser instanceof ConfluenceUserImpl) {
             ConfluenceUserImpl confluenceUser = (ConfluenceUserImpl) atlUser;
-            FieldUtils.writeDeclaredField(confluenceUser, "email", user.getEmail(), true);
-            userManager.saveUser(confluenceUser);
+            try {
+                //email field can only be modified through reflection because it is private
+                FieldUtils.writeDeclaredField(confluenceUser, "email", user.getEmail(), true);
+                userManager.saveUser(confluenceUser);
+            } catch (IllegalAccessException e) {
+                //field "email" is only available from v6.15.10, for backwards compatibility try with backingUser
+                log.debug("didi not find field 'email' in class ConfluenceUserImpl, trying with field 'emailAddress' of backingUser...");
+                User backingUser = confluenceUser.getBackingUser();
+                if (backingUser instanceof EmbeddedCrowdUser) {
+                    EmbeddedCrowdUser crowdUser = (EmbeddedCrowdUser) backingUser;
+                    FieldUtils.writeDeclaredField(crowdUser, "emailAddress", user.getEmail(), true);
+                    userManager.saveUser(crowdUser);
+                } else {
+                    throw new NotImplementedException("no handler implemented for type " + backingUser.getClass());
+                }
+            }
             return getUser(user.getUsername());
         } else {
             throw new UserNotFoundException(user.getUsername());
